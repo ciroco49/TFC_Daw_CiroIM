2f7a49bebf76f5833eda7bd4cf34690ae9a70e52
ciroiencom <ciroiencom@hotmail.com>
2025-03-31 16:44:20 +0200

    Inicialización proyecto con Springboot

---
+++ gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/GamingHeavenTfcApplication.java
@@ -0,0 +1,13 @@
+package com.ciroiencom.gamingheaventfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingHeavenTfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingHeavenTfcApplication.class, args);
+	}
+
+}
---
+++ gamingheaventfc/src/test/java/com/ciroiencom/gamingheaventfc/GamingHeavenTfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciroiencom.gamingheaventfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingHeavenTfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
