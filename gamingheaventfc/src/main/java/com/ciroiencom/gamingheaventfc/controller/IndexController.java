dca69b22495b2abc8535aa3680c6aefc9450e36f
ciroiencom <ciroiencom@hotmail.com>
2025-04-19 14:26:10 +0200

    Estructura html de los card-bodys para los juegos

--- gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/controller/IndexController.java
+++ gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/controller/IndexController.java
@@ -3,13 +3,14 @@ package com.ciroiencom.gamingheaventfc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
     @GetMapping("")
-    public String getIndex() {
-        return "index";
+    public String getIndex(Model model) {
+        return "pages/index";
     }
 }
