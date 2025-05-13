b35e10c415b9f7e20cdc91b2cd5cdb2a35e41791
ciroiencom <ciroiencom@hotmail.com>
2025-05-13 17:02:37 +0200

    Implementación validaciones registro con js y html

--- gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/controller/RegisterController.java
+++ gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/controller/RegisterController.java
@@ -6,10 +6,7 @@ import com.ciroiencom.gamingheaventfc.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,19 +20,20 @@ public class RegisterController {
 
     @GetMapping("")
     public String getIndex(Model model) {
+        model.addAttribute("usuario", new Usuario());
         return "pages/register";
     }
 
     @PostMapping("/saveUser")
-    public String saveUser(@RequestParam String nickname,
-                           @RequestParam String email,
-                           @RequestParam String pass) {
+    public String saveUser(@ModelAttribute Usuario user) {
         try {
             InputStream inStream = getClass().getResourceAsStream("/static/images/default_user.png");
             byte[] imgBytes = inStream.readAllBytes();
 
-            Usuario usr = new Usuario(nickname, email, pass, Rol.USER, imgBytes);
-            usuarioService.save(usr);
+            //Usuario usr = new Usuario(nickname, email, pass, Rol.USER, imgBytes);
+            user.setImg(imgBytes);
+            user.setRol(Rol.USER);
+            usuarioService.save(user);
             return "redirect:/";
         } catch (IOException e) {
             throw new RuntimeException(e);
