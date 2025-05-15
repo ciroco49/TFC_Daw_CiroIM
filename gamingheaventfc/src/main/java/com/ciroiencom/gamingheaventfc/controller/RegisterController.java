4a722f89e320fec1db064a80017fa12f167d859d
ciroiencom <ciroiencom@hotmail.com>
2025-05-15 22:41:15 +0200

    Cambio forma validación de campos en formulario registo

--- gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/controller/RegisterController.java
+++ gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/controller/RegisterController.java
@@ -2,12 +2,14 @@ package com.ciroiencom.gamingheaventfc.controller;
 
 import com.ciroiencom.gamingheaventfc.model.Rol;
 import com.ciroiencom.gamingheaventfc.model.Usuario;
+import com.ciroiencom.gamingheaventfc.model.ValidationGroups;
 import com.ciroiencom.gamingheaventfc.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -31,7 +33,7 @@ public class RegisterController {
     }
 
     @PostMapping("/saveUser")
-    public String saveUser(@Valid @ModelAttribute("usuario") Usuario user, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    public String saveUser(@Validated(ValidationGroups.Register.class) @ModelAttribute("usuario") Usuario user, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         if(bindingResult.hasErrors()) {
             redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.usuario", bindingResult);
--- gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/model/Usuario.java
+++ gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/model/Usuario.java
@@ -25,19 +25,22 @@ public class Usuario implements Serializable {
 
     @NotBlank(message = "Email can´t be empty")
     @Pattern(regexp = "^[a-zA-Z0-9.!¡#$%&'*+\\/=¿?^_`\\{|\\}~-]{1,64}@([a-zA-Z0-9-]{1,63}\\.){1,}[a-zA-Z]{2,63}$"
-            , message = "The email must be in the usual email format and have a maximum length of 320 characters.")
+            , message = "The email must be in the usual email format and have a maximum length of 320 characters."
+            , groups = {ValidationGroups.Register.class, ValidationGroups.Login.class})
     @Column(name = "correo")
     private String correo;
 
     @NotBlank(message = "Password can´t be empty")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)[\\w@\\.,\\/!?#$%^&*()-+=]{8,12}$"
-            , message = "The password must contain a capital letter, a number and be between 8 and 12 characters long.")
+            , message = "The password must contain a capital letter, a number and be between 8 and 12 characters long."
+            , groups = {ValidationGroups.Register.class, ValidationGroups.Login.class})
     @Column(name = "password")
     private String password;
 
     @NotBlank(message = "Nickname can´t be empty")
     @Pattern(regexp = "^[A-Z][a-zA-Z0-9_]{0,19}$"
-            , message = "The nickname must start with a capital letter and have a maximum length of 20 characters.")
+            , message = "The nickname must start with a capital letter and have a maximum length of 20 characters."
+            , groups = {ValidationGroups.Register.class})
     @Column(name = "nickname")
     private String nickname;
 
