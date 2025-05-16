86ed0871e58db46f582967566df04ca60155cca0
ciroiencom <ciroiencom@hotmail.com>
2025-05-16 19:05:15 +0200

    Fix merge develop into feature-login with duplicate fix of users

---
+++ gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/config/SecurityConfig.java
@@ -0,0 +1,91 @@
+package com.ciroiencom.gamingheaventfc.config;
+
+import com.ciroiencom.gamingheaventfc.model.Rol;
+import com.ciroiencom.gamingheaventfc.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .sessionManagement(session -> session
+                        .invalidSessionUrl("/")
+                        .maximumSessions(2)
+                        .maxSessionsPreventsLogin(false)
+                        .expiredUrl("/login?sessionExpired"))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/reloadBBDD").hasRole("ADMIN")
+                        .requestMatchers("/social",
+                                "/social/*",
+                                "/*/likeGame",
+                                "/*/favGame",
+                                "/*/comment",
+                                "/profile",
+                                "/profile/*",
+                                "/logout").authenticated()
+                        .requestMatchers("/",
+                                "/login",
+                                "/login/*",
+                                "/register",
+                                "/register/*",
+                                "/*",
+                                "/static/**",
+                                "/css/**",
+                                "/images/**",
+                                "/js/**").permitAll()
+                        .anyRequest().denyAll())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?errorLogin")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .clearAuthentication(true))
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(CustomUserDetailsService customUserDetailsService) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(customUserDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.ciroiencom.gamingheaventfc.service;
+
+import com.ciroiencom.gamingheaventfc.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.findUserByNickname(username);
+        if(usuario == null) throw new UsernameNotFoundException("El usuario proporcionado -> " + username + " <- no existe");
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + usuario.getRol().name()));
+
+        return new User(usuario.getNickname(), usuario.getPassword(), authorities);
+    }
+}
