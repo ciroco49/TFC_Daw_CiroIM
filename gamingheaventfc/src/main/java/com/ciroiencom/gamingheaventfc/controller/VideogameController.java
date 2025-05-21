6fe9dea16c73e1efde639551b6c7d7b5bcf50ecf
ciroiencom <ciroiencom@hotmail.com>
2025-05-21 15:34:56 +0200

    Implementación de validaciones front y back para comentarios

--- gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/controller/VideogameController.java
+++ gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/controller/VideogameController.java
@@ -6,15 +6,19 @@ import com.ciroiencom.gamingheaventfc.model.Usuario;
 import com.ciroiencom.gamingheaventfc.service.ComentariosService;
 import com.ciroiencom.gamingheaventfc.service.JuegoService;
 import com.ciroiencom.gamingheaventfc.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Base64;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Controller
 @RequestMapping("/")
@@ -39,6 +43,9 @@ public class VideogameController {
             return "pages/videogame";
         }
 
+        if(model.containsAttribute("commentLength"))
+            model.addAttribute("commentLength", model.getAttribute("commentLength"));
+
         Usuario user = usuarioService.findByNickname(userLogged.getUsername());
         model.addAttribute("imgUser", Base64.getEncoder().encodeToString(user.getImg()));
 
@@ -46,7 +53,16 @@ public class VideogameController {
     }
 
     @PostMapping("/{title}/comment")
-    public String saveComment(@RequestParam String texto, @PathVariable String title, @AuthenticationPrincipal User userLogged) {
+    public String saveComment(@RequestParam String texto, RedirectAttributes redirectAttributes,
+                              @PathVariable String title, @AuthenticationPrincipal User userLogged) {
+
+        String textPattern = "^.{1,500}$";
+        //Si el title no cumple el pattern
+        if(!Pattern.compile(textPattern, Pattern.MULTILINE).matcher(title).find()) {
+            redirectAttributes.addFlashAttribute("commentLength", "The comment must have between 1 and 500 characters.");
+            return "redirect:/" + title;
+        }
+
         Usuario user = usuarioService.findByNickname(userLogged.getUsername());
         Juego videogame = juegoService.findByTitle(title);
 
--- gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/model/Comentarios.java
+++ gamingheaventfc/src/main/java/com/ciroiencom/gamingheaventfc/model/Comentarios.java
@@ -1,6 +1,7 @@
 package com.ciroiencom.gamingheaventfc.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.io.Serializable;
