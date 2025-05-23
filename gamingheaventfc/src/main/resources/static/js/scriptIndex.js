08361d1365a22dae38489d58794243e1355196a0
ciroiencom <ciroiencom@hotmail.com>
2025-05-23 13:45:09 +0200

    Visualizar favs en perfil y cambios y refactorización de ordenar método utils común

--- gamingheaventfc/src/main/resources/static/js/scriptIndex.js
+++ gamingheaventfc/src/main/resources/static/js/scriptIndex.js
@@ -1,3 +1,5 @@
+import { setThumbnails } from './scriptUtils.js';
+
 setThumbnails();
 const videogame_card_container = document.getElementById("videogame_card_container");
 const videogames = Array.from(videogame_card_container.children);
@@ -6,65 +8,6 @@ const selectOrderBy = document.getElementById("selectOrderBy");
 // ================== LISTENERS ================================
 
 selectOrderBy.addEventListener("change", orderBy);
-
-// =============================================================
-
-
-
-// ============== SET THUMBNAILS ===============================
-
-async function setThumbnails() {
-    const thumbnailsMap = await getThumbnailsFromApi();
-    
-    try {
-        for(const [idApi, url] of thumbnailsMap) {
-              var img = document.getElementById(idApi+"img");
-              if(img != null) {
-                img.setAttribute("src", url);
-                img.className = "w-100 h-100";
-              } else{
-                console.log("Videojuego con idAPI " + idApi + " no guardado en BBDD");
-              }
-            }
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
-async function getThumbnailsFromApi() {
-    const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': 'da2e264c31msh0f5a76c9f3898c9p1fc2f1jsnd383c1050290',
-            'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await fetch(url, options);
-
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
-        }
-    
-        const json = await response.json();
-        const thumbnailsMap = new Map();
-        
-        for (const videogame of json) {
-            thumbnailsMap.set(String(videogame.id), videogame.thumbnail);
-        }
-
-        return thumbnailsMap;
-      } catch (error) {
-        console.error(error.message);
-      }
-      
-}
-
-// =============================================================
-
-
 
 // ============== FILTER GAMES WITH CHECKBOX ===================
 
